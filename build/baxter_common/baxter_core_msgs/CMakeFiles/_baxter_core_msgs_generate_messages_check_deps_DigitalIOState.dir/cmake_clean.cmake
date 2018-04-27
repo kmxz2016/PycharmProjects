ffdf665a72d4380e3c10baf9e1acad5c193c81ae
MIN <601985329@qq.com>
2018-04-27 16:05:10 +0800

    27

--- build/baxter_common/baxter_core_msgs/CMakeFiles/_baxter_core_msgs_generate_messages_check_deps_DigitalIOState.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/_baxter_core_msgs_generate_messages_check_deps_DigitalIOState"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/_baxter_core_msgs_generate_messages_check_deps_DigitalIOState.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
